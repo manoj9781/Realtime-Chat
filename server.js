cbef072be29a8467f29f5f9e03e4db16b23493d4
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-15 19:26:11 +0530

    Create server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
 
 const app = express();
 
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, function () {
+server.listen(PORT, function () {
     console.log("Server is running on the port", PORT);
 })
