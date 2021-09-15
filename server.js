aed0cd4fa0842fc44bb34fb5646b14a1a139c8d8
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-15 23:24:37 +0530

    Connection sucessful for socket.io

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
 
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log("New connection");
+
+    socket.emit('message', 'Welcome to Realtime Chat');
+})
 
 const PORT = 3000 || process.env.PORT;
 
