ca42cd7b71aec3a0c829d9862bdc4231b647edc9
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-16 23:01:35 +0530

    User Function done

---
+++ utils/users.js
@@ -0,0 +1,22 @@
+const users = [];
+
+// User Join the chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+
+  users.push(user);
+  return user;
+}
+
+// Get the current user
+
+function getCurrentUser(id) {
+  return users.find((user) => {
+    user.id === id;
+  });
+}
+
+module.exports = {
+    userJoin,
+    getCurrentUser
+}
